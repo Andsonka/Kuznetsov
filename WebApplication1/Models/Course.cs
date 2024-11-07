09221087084b7f691d7b5c70c4e8316693326f09
Andsonka <alesha_kuznetsov_03@inbox.ru>
2024-11-07 17:33:07 +0300

    3 table

---
+++ WebApplication1/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApplication1.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string? Title { get; set; }
+        public int GroupId { get; set; }
+        [JsonIgnore]
+        public Group? Group { get; set; }
+    }
+}
